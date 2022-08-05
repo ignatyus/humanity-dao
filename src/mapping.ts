f46a823c9f2f4a77a2d2237f074516b6e0b6d8b9
ignatyus <ignatyus@outlook.com>
2022-08-05 16:02:58 +0300

    Completed the handleExecute and handleTerminate functions Fixed issues with handlers so that now they save the changes made to the entities Fixed issues with handlers so that now User entity is also created correctly

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, log } from "@graphprotocol/graph-ts"
 
 import {
     Execute,
@@ -17,50 +17,142 @@ import {
 
 export function handlePropose(event: Propose): void {
     let proposalID = event.params.proposalId
-    let proposal = Proposal.load (proposalID.toString())
     let proposer = event.params.proposer
-    proposal.result = "Pending"
-    proposal.proposer = proposer.toString()
-    proposal.startTimestamp = event.block.timestamp
-    proposal.yesCount = HumanityGovernance.bind (event.address).yesVotes(proposalID, proposer)
-    proposal.noCount = BigInt.fromI32 (0)
-    proposal.isFinalized = false
+    let proposerID = proposer.toString()
+
+    let proposal = Proposal.load (proposalID.toString())
+    
+    if (proposal == null) {
+        proposal = new Proposal (proposalID.toString())
+
+        proposal.result = "Pending"
+        proposal.proposer = proposerID
+        proposal.startTimestamp = event.block.timestamp
+        proposal.yesCount = HumanityGovernance.bind (event.address).yesVotes(proposalID, proposer)
+        proposal.noCount = BigInt.fromI32 (0)
+        proposal.isFinalized = false
+        proposal.save()
+
+        let user = User.load (proposerID)
+        if (user == null) {
+            user = new User (proposerID)
+            user.save()
+        }
+    }
+    else {
+        log.warning ("This proposal already exists. ID {}", [proposalID.toString()])
+    }
+    
+    
 
     proposal.save()
 }
 
 export function handleVote(event: Vote): void {
-    /*
-    let vote = EntityVote.load(event.params.proposalId.toString() + event.params.voter.toString())
-    
-    if (vote != null) {
-        let voter = User.load (event.params.voter.toHexString())
-        vote.voter = voter.id
-        vote.weight = event.params.weight
+    let voterID = event.params.voter.toString()
+    let proposalID = event.params.proposalId.toString()
+    let proposal = Proposal.load (proposalID)
+
+    if (proposal == null) {
+        log.warning ("This proposal does not exist. ID {}", [proposalID])
     }
-    */
+    else {
+        let count = 0
+        for (let v in proposal.votes) {
+            let vote = EntityVote.load (v)
+            if (vote != null && vote.voter == voterID) {
+                count ++
+            }
+        }
+
+        let voteID = proposalID.concat ("-").concat (voterID).concat ("-").concat (count.toString())
+        let vote = new EntityVote (voteID)
+
+        vote.voter = voterID
+        vote.proposal = proposalID
+        vote.timestamp = event.block.timestamp
+        vote.option = event.params.approve ? "Yes" : "No"
+        vote.weight = event.params.weight
+        vote.removed = false
+
+        vote.save()
+
+        if (vote.option == "Yes") {
+            proposal.yesCount = proposal.yesCount.plus(vote.weight )
+        }
+        else if (vote.option == "No") {
+            proposal.noCount = proposal.noCount.plus(vote.weight )
+        }
+
+        proposal.save()
+
+        let user = User.load (voterID)
+        if (user == null) {
+            user = new User (voterID)
+            user.save()
+        }
+    }
+    
 }
 
 export function handleRemoveVote(event: RemoveVote): void {
-    
+    let voterID = event.params.voter.toString()
+    let proposalID = event.params.proposalId.toString()
+    let proposal = Proposal.load (proposalID)
+
+    if (proposal == null) {
+        log.warning ("This proposal does not exist. ID {}", [proposalID])
+    }
+    else {
+        for (let count = 0; count < proposal.votes.length, count++;) {
+            let voteID = proposalID.concat ("-").concat (voterID).concat ("-").concat (count.toString())
+            let vote = EntityVote.load(voteID)
+
+            if (vote != null) {
+                vote.removed = true
+                vote.save()
+
+                if (vote.option == "Yes") {
+                    proposal.yesCount = proposal.yesCount.minus (vote.weight)
+                }
+                else if (vote.option == "No") {
+                    proposal.noCount = proposal.noCount.minus(vote.weight )
+                }
+                
+                proposal.save()
+            }
+            else {
+                break;
+            }
+        }
+    }
 }
 
 export function handleExecute(event: Execute): void {
-    /*
-    let proposal = Proposal.load(event.params.proposalId.toString())
+    let proposalID = event.params.proposalId.toString()
+    let proposal = Proposal.load (proposalID)
 
     if (proposal != null) {
-        proposal.result = "True"
+        proposal.result = "Yes"
+        proposal.isFinalized = true
         proposal.save()
     }
     else {
-
+        log.warning ("This proposal does not exist. ID {}", [proposalID])
     }
-    */
 }
 
 
 export function handleTerminate(event: Terminate): void {
-    
-}
+    let proposalID = event.params.proposalId.toString()
+    let proposal = Proposal.load (proposalID)
 
+    if (proposal != null) {
+        proposal.result = "No"
+        proposal.isFinalized = true
+        proposal.save()
+    }
+    else {
+        log.warning ("This proposal does not exist. ID {}", [proposalID])
+    }
+}
