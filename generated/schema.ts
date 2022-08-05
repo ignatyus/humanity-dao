3ca425f2b801479724ee203f57ddf0749ffe8197
ignatyus <ignatyus@outlook.com>
2022-08-05 16:02:29 +0300

    Added timestamp field to VoteEntity

--- generated/schema.ts
+++ generated/schema.ts
@@ -205,6 +205,15 @@ export class EntityVote extends Entity {
     this.set("proposal", Value.fromString(value));
   }
 
+  get timestamp(): BigInt {
+    let value = this.get("timestamp");
+    return value!.toBigInt();
+  }
+
+  set timestamp(value: BigInt) {
+    this.set("timestamp", Value.fromBigInt(value));
+  }
+
   get option(): string {
     let value = this.get("option");
     return value!.toString();
